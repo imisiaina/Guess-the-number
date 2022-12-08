4f8cbc3cd772df7d559e317b52e69c38e623bdb6
imisiaina <imis.aina1@gmail.com>
2022-12-08 11:35:48 +0000

    Added Project

---
+++ Guess_the_number.py
@@ -0,0 +1,22 @@
+import random
+import math
+
+secret_number = math.floor(random.randint(1, 10))
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input('Guess: '))
+    guess_count += 1
+    if guess == secret_number:
+        print('You Won')
+        break
+else:
+    print('Sorry, you failed!')
+    print(f'The number was: {secret_number}')
+
+
+
+
+
+
+
